9f1c7e5aad5ff2e102e23f8415a107fe9fd947a7
Jay Kinzie <jaykinzie@gmail.com>
2021-02-03 18:33:30 -0600

    more UI. Opening and saving files. Proved that QR codes can be used to send and recieve information

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ import shutil
 import cv2
 from pyzbar import pyzbar
 
+from requests import get
+
+
 
 class WireGuardInterface():
     def __init__(self):
@@ -23,21 +26,112 @@ class WireGuardInterface():
         self.interface_private_key = ""
         self.interface_ip = "000.000.000.000"
         self.interface_listen_port = 00000
+        self.qr_code = None
+
+    def generate_interface_qr_code(self):
+
+        # if a private key does not exist, generate it
+        if os.path.exists("private_key.txt"):
+            private_key_file = open("private_key.txt")
+            self.interface_private_key = private_key_file.read()
+        else:
+            private_key_file = open("private_key.txt", mode='w')
+            # dummy random string to use until hooked into wireguard
+            s = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+            private_key_file.write(s)
+            self.interface_private_key = s
+
+        # if a public key does not exist, generate it
+        if os.path.exists("public_key.txt"):
+            public_key_file = open("public_key.txt")
+            self.interface_public_key = public_key_file.read()
+        else:
+            public_key_file = open("public_key.txt", mode='w')
+            # dummy random string to use until hooked into wireguard
+            s = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+            public_key_file.write(s)
+            self.interface_public_key = s
+
+        # get the current external ip address
+        self.interface_ip = get('https://api.ipify.org').text
+
+        self.interface_listen_port = 12345
+
+        # create qr code
+        qr = qrcode.QRCode(version=1, box_size=10, border=1)
+        # add data to the QR code
+        qr.add_data(self.interface_ip + ":" +
+                    str(self.interface_listen_port) + ":" +
+                    self.interface_public_key)
+
+        # compile the data into a QR code array
+        qr.make()
+        print("The shape of the QR image:", np.array(qr.get_matrix()).shape)
+        # transfer the array into an actual image
+        self.qr_code = qr.make_image(fill_color="black", back_color="white")
+
+        # save the image
+        if os.path.exists("qr_codes/interface_qr_code.png"):
+            os.remove("qr_codes/interface_qr_code.png")
+        self.qr_code.save("qr_codes/interface_qr_code.png")
 
 
-class WireGuardPeer():
+class WireGuardPeer:
     def __init__(self):
         # personal private key
         self.peer_public_key = ""
         self.peer_ip = "000.000.000.000"
         self.peer_listen_port = 00000
+        self.qr_code = None
+
+    def make_dummy(self):
+        self.peer_public_key = "wU4oVpiPHZAuUSzqfEutpK5yZp9t9tpTFIWPL3/2KA0="
+        self.peer_ip = "123.456.789.123"
+        self.peer_listen_port = 12345
+        self.generate_peer_qr_code()
+
+    def generate_peer_qr_code(self):
+
+        # if a public key does not exist, generate it
+        if os.path.exists("incoming_qr_codes/peer_public_key.txt"):
+            public_key_file = open("incoming_qr_codes/peer_public_key.txt")
+            self.peer_public_key = public_key_file.read()
+        else:
+            public_key_file = open("incoming_qr_codes/peer_public_key.txt", mode='w')
+            # dummy random string to use until hooked into wireguard
+            s = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+            public_key_file.write(s)
+            self.peer_public_key = s
+
+        # get the current external ip address
+        self.peer_ip = get('https://api.ipify.org').text
+
+        self.peer_listen_port = 12345
+
+        # create qr code
+        qr = qrcode.QRCode(version=1, box_size=10, border=1)
+        # add data to the QR code
+        qr.add_data(self.peer_ip + ":" +
+                    str(self.peer_listen_port) + ":" +
+                    self.peer_public_key)
+
+        # compile the data into a QR code array
+        qr.make()
+        print("The shape of the QR image:", np.array(qr.get_matrix()).shape)
+        # transfer the array into an actual image
+        self.qr_code = qr.make_image(fill_color="black", back_color="white")
+
+        # save the image
+        if os.path.exists("incoming_qr_codes/peer_qr_code.png"):
+            os.remove("incoming_qr_codes/peer_qr_code.png")
+        self.qr_code.save("incoming_qr_codes/peer_qr_code.png")
 
 
-class MainUi():
+class MainUi:
     def __init__(self, root):
 
+        # the root window
         self.root = root
-
         # empty out the qr code directory
         self.clean_qr_directory()
         # create a qr code and then safe the location
@@ -87,6 +181,7 @@ class MainUi():
         qr = qrcode.QRCode(version=1, box_size=10, border=1)
         # add data to the QR code
         qr.add_data(data)
+
         # compile the data into a QR code array
         qr.make()
         # print the image shape
@@ -146,15 +241,39 @@ def read_barcodes(frame):
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)  # 3
 
-        with open("qr_codes/barcode_result.txt", mode='w') as file:
+        with open("incoming_qr_codes/barcode_result.txt", mode='w') as file:
             file.write("Recognized Barcode:" + barcode_info)
+            parse_barcodes(barcode_info)
             # if we find a barcode, return false to stop the loop and the frame
             return False, frame
     # if nothing is found, return true and the frame to keep the while loop running
     return True, frame
 
 
+def parse_barcodes(barcode):
+
+    valid_Ip = False
+    Valid_Port = False
+    Valid_Public_Key = False
+
+    barcodes = []
+    barcodes = barcode.split(':')
+    print(barcodes)
+
+    # if the barcode is valid then return true
+
+
+
+
 def main():
+
+    # dummy to generate internal qr code
+    personal_interface = WireGuardInterface()
+    personal_interface.generate_interface_qr_code()
+
+    peer_qr_code = WireGuardPeer()
+    peer_qr_code.make_dummy()
+
     # define the main window which is the size of the screen on the pi
     root = tk.Tk()
     root.title("Pi-Guard-Appliance")
